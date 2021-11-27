a007d8e2f44bad8429d2a92be76cdd8bc836de40
Cameron Kilgore <ghostfreeman@gmail.com>
2021-11-27 10:54:09 -0600

    Initial work on static markup and CSS

--- moonshot-travels/src/App.js
+++ moonshot-travels/src/App.js
@@ -5,19 +5,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Book a trip</h1>
+        <p>An amazing journey is waiting for you and your loved ones! You’re just one step away from a new adventure:</p>
       </header>
+
+      <div className="AppForm">
+        <form action="#" method="post">
+          <p>
+            <label for='name'>Your Name</label>
+            <input type="text" name='name' id='name'></input>
+          </p>
+          <p>
+            <label for='email'>E-mail Address</label>
+            <input type='text' name='email' id='email'></input>
+          </p>
+          <p>
+            <label for='phone'>Phone Number</label>
+            <input type="text" name="phone" id="phone"></input>
+          </p>
+
+          <ul>
+            <li>
+              <label for="adults">Adults</label>
+              <input type="number" name="adults" id="adults"></input>
+            </li>
+            <li>
+              <label for="kids">Kids</label>
+              <input type="number" name="kids" id="kids"></input>
+            </li>
+            <li>
+              <label for="cabins">Cabin Rooms</label>
+              <input type="number" name="cabins" id="cabins"></input>
+            </li>
+            <div className="formSubmit">
+              <button>Complete Your Booking</button>
+            </div>
+          </ul>
+        </form>
+      </div>
     </div>
   );
 }
